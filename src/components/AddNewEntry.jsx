e9b97ed8424fe71807cd79b52e73301e77f310d8
Yahya-Pak <syedmyahya786@gmail.com>
2025-07-16 10:14:30 +0500

    Broken Features

---
+++ src/components/AddNewEntry.jsx
@@ -0,0 +1,70 @@
+import React from 'react'
+
+const AddNewEntry = () => {
+  return (
+    <div>
+      
+        {/* <div className="text-black flex flex-col p-4 gap-8 items-center">
+          <input
+            placeholder="Enter Website URL"
+            value={form.site}
+            onChange={handleChange}
+            className="rounded-full bg-white border border-green-500 w-full p-4 py-1 text-black focus:outline-none focus:ring-1 focus:ring-green-500"
+            type="text"
+            name="site"
+            id="site"
+          />
+          <div className="flex flex-col md:flex-row w-full gap-8 justify-between">
+            <input
+              placeholder="Enter Username"
+              value={form.username}
+              onChange={handleChange}
+              className="bg-white rounded-full border border-green-500 w-full p-4 py-1 text-black focus:outline-none focus:ring-1 focus:ring-green-500"
+              type="text"
+              name="username"
+              id="user"
+            />
+            <div className="relative ">
+              <input
+                ref={passwordRef}
+                placeholder="Enter Password"
+                value={form.password}
+                onChange={handleChange}
+                className="bg-white rounded-full border border-green-500 w-full p-4 py-1 text-black focus:outline-none focus:ring-1 focus:ring-green-500"
+                type="password"
+                name="password"
+                id="pass"
+              />
+
+              <span
+                className="absolute top-[4px] right-[3px] cursor-pointer"
+                onClick={showPassword}
+              >
+                <img
+                  ref={ref}
+                  className="p-1"
+                  width={28}
+                  src="icons/eye.png"
+                  alt="eye"
+                />
+              </span>
+            </div>
+          </div>
+
+          <button
+            className="flex justify-center items-center gap-2 bg-green-500 rounded-full px-8 py-2 w-fit
+           hover:bg-green-400 border border-green-700"
+            onClick={savePassword}
+          >
+            <lord-icon
+              src="https://cdn.lordicon.com/efxgwrkc.json"
+              trigger="hover"
+            ></lord-icon>
+            Save
+          </button>
+        </div> */}
+    </div>
+  )
+}
+
+export default AddNewEntry
